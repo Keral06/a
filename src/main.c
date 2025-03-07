f3862265672f0265a21f8cd70e32a413bda7c01e
Keral06 <queralt.escutia@estudiantat.upc.edu>
2025-03-07 08:31:27 +0100

    .

--- src/main.c
+++ src/main.c
@@ -23,30 +23,44 @@ int main ()
 	SearchAndSetResourceDir("resources");
 
 	// Load a texture from the resources directory
-	Texture wabbit = LoadTexture("wabbit_alpha.png");
+	Texture logo = LoadTexture("pene.png");
 	
+	const int screenWidth = 800;
+	const int screenHeight = 450;
+	SetTargetFPS(60);
+
+	int seconds = 4;
+
 	// game loop
 	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
 	{
+
+
 		// drawing
 		BeginDrawing();
 
 		// Setup the back buffer for drawing (clear color and depth buffers)
-		ClearBackground(GREEN);
+		ClearBackground(BLACK);
 
 		// draw some text using the default font
-		DrawText("Hello Raylib", 200,200,20,WHITE);
+		DrawText("Irene, Jiayi & Queralt", 300,200,20,WHITE);
 
 		// draw our texture to the screen
-		DrawTexture(wabbit, 400, 200, WHITE);
-		
+		DrawTexture(logo,350, 320, WHITE);
+
+		double GetTime(seconds);
+		void WaitTime(GetTime);
+
+		DrawRectangle(screenWidth / 4 * 2 - 60, 100, 800, 900, RED);
 		// end the frame and get ready for the next one  (display frame, poll input, etc...)
 		EndDrawing();
+
+
 	}
 
 	// cleanup
 	// unload our texture so it can be cleaned up
-	UnloadTexture(wabbit);
+	UnloadTexture(logo);
 
 	// destroy the window and cleanup the OpenGL context
 	CloseWindow();
