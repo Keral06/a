9c3e9430b01340a97e294f8a1224bd2d8a6a12a6
irentren <icalatravasierra@gmail.com>
2025-03-21 08:46:54 +0100

    aaaa

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,68 @@
-#include "Game.h"
-#include "MemLeaks.h"
-#include "Globals.h"
+/*
+Raylib example file.
+This is an example main file for a simple raylib project.
+Use this as a starting point or replace it with your code.
 
-int main()
+by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit https://creativecommons.org/publicdomain/zero/1.0/
+
+*/
+
+#include "raylib.h"
+
+#include "resource_dir.h"	// utility header for SearchAndSetResourceDir
+
+int main ()
 {
-    ReportMemoryLeaks();
+	// Tell the window to use vsync and work on high DPI displays
+	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
-    Game *game;
-    AppStatus status;
-    int main_return = EXIT_SUCCESS;
+	// Create the window and OpenGL context
+	InitWindow(1280, 800, "Hello Raylib");
 
-    LOG("Application start");
-    game = new Game();
-    status = game->Initialise(GAME_SCALE_FACTOR);
-    if (status != AppStatus::OK)
-    {
-        LOG("Failed to initialise game");
-        main_return = EXIT_FAILURE;
-    }
-        
-    while (status == AppStatus::OK)
-    {
-        status = game->Update();
-        if(status != AppStatus::OK)
-        {
-            if(status == AppStatus::ERROR)      main_return = EXIT_FAILURE;
-            else if(status == AppStatus::QUIT)  main_return = EXIT_SUCCESS;
-            break;
-        }
-        game->Render();
-    }
+	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
+	SearchAndSetResourceDir("resources");
 
-    LOG("Application finish");
-    game->Cleanup();
- 
-    LOG("Bye :)");
-    delete game;
+	// Load a texture from the resources directory
+	Texture logo = LoadTexture("pene.png");
+	
+	const int screenWidth = 800;
+	const int screenHeight = 450;
+	SetTargetFPS(60);
 
-    return main_return;
+	int seconds = 4;
+
+	// game loop
+	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
+	{
+
+
+		// drawing
+		BeginDrawing();
+
+		// Setup the back buffer for drawing (clear color and depth buffers)
+		ClearBackground(BLACK);
+
+		// draw some text using the default font
+		DrawText("Irene, Jiayi & Queralt", 300,200,20,WHITE);
+
+		// draw our texture to the screen
+		DrawTexture(logo,350, 320, WHITE);
+
+		double GetTime(seconds);
+		void WaitTime(GetTime);
+
+		DrawRectangle(screenWidth / 4 * 2 - 60, 100, 800, 900, RED);
+		// end the frame and get ready for the next one  (display frame, poll input, etc...)
+		EndDrawing();
+
+
+	}
+
+	// cleanup
+	// unload our texture so it can be cleaned up
+	UnloadTexture(logo);
+
+	// destroy the window and cleanup the OpenGL context
+	CloseWindow();
+	return 0;
 }
