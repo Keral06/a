2a28d4c0659f5bfafd7e3d299b35d348c55bbf6c
irentren <icalatravasierra@gmail.com>
2025-04-21 18:14:07 +0200

    p

--- src/main.cpp
+++ src/main.cpp
@@ -1295,7 +1295,7 @@ public:
 
                 bullets[i].UpdatePosition(this->level);
                 DrawTexture(bulletTex, bullets[i].GetPosition().x, bullets[i].GetPosition().y, WHITE);
-                i++;
+                
                 if (bullets[i].eliminate == true) {
                 
                     bulletSize = bullets.size();
@@ -1315,7 +1315,7 @@ public:
 
                     }
                     bulletSize = bullets.size();
-                
+                    i++;
                 }
             }
 
